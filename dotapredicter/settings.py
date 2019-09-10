462f8528ee9d418eb96f34172d77a8b3b0aa3fd4
ltropin <ltropin@mail.ru>
2019-09-10 22:34:22 +0500

    staticfiles

--- dotapredicter/settings.py
+++ dotapredicter/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/frontend/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'frontend/static')
 ]
